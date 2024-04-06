e1144fdd957231a7469881ec2c7b30a8a798b5c9
robin.correa <robin.correa21@gmail.com>
2024-04-07 01:43:01 +0800

    First commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet8_mvc_petcare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
