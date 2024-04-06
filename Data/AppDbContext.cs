02aaf52de062310bd1d867132875ba403a7a421b
robin.correa <robin.correa21@gmail.com>
2024-04-07 02:09:03 +0800

    Create Domain model for Services. Create and inject DB context in main application. Install EFC packages.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using dotnet8_mvc_petcare.Models.Domains;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet8_mvc_petcare.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Service> Services { get; set; }
+    }
+}
